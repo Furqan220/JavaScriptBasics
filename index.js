dc24aae5a13c3b56149d84482aaef730f6108ce5
Furqan221 <furqan.ahmed@jumppace.com>
2023-10-12 06:40:56 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+let a = 50 
+let b = 8
+
+// airthmatic operator
+
+console.log("Airthmatic operator")
+
+
+console.log("a = ", a)
+console.log("b = ", b)
+console.log("a + b = ", a+b)
+console.log("a - b = ", a-b)
+console.log("a / b = ", a/b)
+console.log("a x b = ", a*b)
+console.log("a xx b = ", a**b)
+console.log("a % b = ", a%b)
+console.log("a = ", a)
+console.log("++a = ", ++a)
+
+console.log("a++ = ", a++)
+console.log("a = ", a)
+
+
+console.log("b = ", b)
+console.log("--b = ", --b)
+
+console.log("b-- = ", b--)
+console.log("b = ", b)
+
+
+// Logical Operators
+console.log("Logical operator")
+
+console.log("IsEqual => a == b", a == b)
+console.log("IsNotEqual => a != b", a != b)
+
+
